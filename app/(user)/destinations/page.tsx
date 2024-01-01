c026a585c2c83941ea223929e1894634ba9f0441
morachake <jacobmoracha@gmail.com>
2024-01-01 12:05:40 +0300

    updatingdestinations

--- app/(user)/destinations/page.tsx
+++ app/(user)/destinations/page.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { getDestinations } from "../../../sanity/sanity-utils";
 import { urlForImage } from '@/sanity/lib/image';
+import { HiLocationMarker, HiStar } from "react-icons/hi";
 
 export default async function Destinations() {
   const destinations = await getDestinations();
   console.log("your destinations",destinations);
   return (
     <section className="w-full min-h-screen">
-      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 p-16 pb-8 sm:pb-12 flex flex-col space-y-6 mt-10 sm:mt-16 lg:mt-24">
+     <div className="max-w-7xl m-auto px-4 sm:px-6 lg:px-8 p-16 pb-8 sm:pb-12 flex flex-col space-y-6 mt-10 sm:mt-16 lg:mt-24">
         <h2 className="text-4xl">Destinations.</h2>
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-4 pb-8">
           {destinations.map((dest) => (
@@ -16,30 +17,39 @@ export default async function Destinations() {
               href={`/destinations/${dest.slug}`}
               key={dest._id}
             >
-              <p className="w-full max-w-sm border border-gray-200 rounded-lg shadow dark:border-gray-700">
-                <Image 
-                  className="rounded-t-lg" 
-                  layout="responsive"
-                  width={16}
-                  height={9}
+              <div className="w-full relative h-[200px]">
+                 <Image
                   src={urlForImage(dest.image).url()}
-                  alt={dest.name}
-                />
-                <div className="px-5 pb-5">
-                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">{dest.name}</h5>
-                  <div className="flex items-center mt-2.5 mb-5">
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <span className="text-md font-bold text-gray-900 ">${dest.price}</span>
-                  </div>
-                  <div>
-                    <p>{dest.content}</p>
-                  </div>
-                  <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Book Now
-                  </a>
-                </div>
-              </p>
+                   alt="Destination image"
+                   fill
+                   className="rounded-t-md object-cover hover:grayscale transition-all filter duration-300 ease-out"
+              />
+             {/* Stars */}
+                 <div className="p-1 absolute top-1 left-1 bg-white rounded-sm flex flex-row font-bold text-yellow">
+                   <HiStar size={14} />
+                   <HiStar size={14} />
+                   <HiStar size={14} />
+                   <HiStar size={14} />
+                   <HiStar size={14} />
+                 </div>
+              </div>
+             {/* Description */}
+               <div className="flex flex-col gap-2 px-4 pb-6">
+                 <h3 className=" text-xl text-green">{dest.name}</h3>
+
+                 {/* Location and price */}
+                 <div className="flex items-around justify-between text-md bold">
+                   <p className="text-black/70 font-talic">
+                     <span className="inline-block mr-1">
+                       <HiLocationMarker size={20} />
+                     </span>
+                     {dest.name}
+                   </p>
+                   <p className="text-black/70 font-bold">
+                     ${dest.price}
+                   </p>
+                 </div>
+               </div>
             </Link>
           ))}
         </div>
