31c550be86b51a499630c259775f6f50724f6d23
morachake <jacobmoracha@gmail.com>
2024-01-01 22:59:19 +0300

    updting destination cards

--- app/(user)/destinations/page.tsx
+++ app/(user)/destinations/page.tsx
@@ -11,15 +11,16 @@ export default async function Destinations() {
   console.log("your destinations",destinations);
   return (
     <section className="w-full min-h-screen">
-     <div className="max-w-7xl m-auto px-4 sm:px-6 lg:px-8 p-16 pb-8 sm:pb-12 flex flex-col space-y-6 mt-10 sm:mt-16 lg:mt-24">
+     <div className="max-w-6xl m-auto px-4 sm:px-6 lg:px-4 p-16 pb-8 sm:pb-12 flex flex-col space-y-8  mt-10 sm:mt-16 lg:mt-24">
         <h2 className="text-4xl">Destinations.</h2>
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-4 pb-8">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-3 gap-4 pt-4 pb-8">
           {destinations.map((dest) => (
             <Link 
               href={`/destinations/${dest.slug}`}
               key={dest._id}
+              className=' border-2  border-black rounded-md'
             >
-              <div className="w-full relative h-[200px]">
+              <div className="w-full relative h-[300px]">
                  <Image
                   src={urlForImage(dest.image).url()}
                    alt="Destination image"
@@ -37,7 +38,7 @@ export default async function Destinations() {
               </div>
              {/* Description */}
                <div className="flex flex-col gap-2 px-4 pb-6">
-                 <h3 className=" text-xl text-green">{dest.name}</h3>
+                 <h3 className=" text-xl text-green">Destination :{dest.name}</h3>
 
                  {/* Location and price */}
                  <div className="flex items-around justify-between text-md bold">
@@ -45,10 +46,10 @@ export default async function Destinations() {
                      <span className="inline-block mr-1">
                        <HiLocationMarker size={20} />
                      </span>
-                     {dest.name}
+                     City : {dest.name}
                    </p>
                    <p className="text-black/70 font-bold">
-                     ${dest.price}
+                    Price ${dest.price}
                    </p>
                  </div>
                 
